c029eb889bdad6220743011f03c37acded486483
Devin <dbidwell94@gmail.com>
2020-05-19 21:32:08 -0700

    Updated tweet view to use REST API

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
 
 def home_page(request, *args, **kwargs):
-    pass
+    print(args)
+    print(kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST Api view: return JSON data
+    Consume by JavaScript
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        tweet = Tweet.objects.get(id=tweet_id)
+        data['content'] = tweet.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
